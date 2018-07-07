5c4d09c8c46e8012dacf3ffec9838f5220363c68
Cemal Biryol <cbbiryol@Cemals-MacBook-1774.local>
2018-07-07 14:51:44 -0400

    all done v3

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var mongoose = require("mongoose");
 // Body Parser 
 var bodyParser = require("body-parser");
 
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 // Initialize Express
 var app = express();
